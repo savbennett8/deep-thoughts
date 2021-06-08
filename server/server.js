f32af4d858d30af32432b2e97c1d9a4e0440d23d
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-07 18:54:59 -0600

    implemented typeDefs & resolvers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+//create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//integrate our Apollo server w/Express application as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +21,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //where you can go to test the GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
