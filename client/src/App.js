8b20e56661f89c69444796d2cc5b1d74c6f2c04d
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-10 21:33:18 -0600

    setup React Router to route the page & render components

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,18 @@ import {
   //controls how ApolloClient makes a request(kind of like middleware)
   createHttpLink
 } from "@apollo/client";
+//rename 'BrowserRouter' to 'Router'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   // uri: 'http://localhost:3001/graphql',
@@ -28,13 +35,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/profile" component={Profile} />
+              <Route exact path="/thought" component={SingleThought} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
