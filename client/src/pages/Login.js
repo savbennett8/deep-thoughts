190a52051069d5fc72de4fd0f7b7751dd64f52e6
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-14 12:41:15 -0600

    login & signup mutations implemented

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -22,6 +25,16 @@ const Login = (props) => {
       email: '',
       password: '',
     });
+
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -53,6 +66,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
