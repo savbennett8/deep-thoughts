1857b78817466dc6f7ac7e9791db410acf4ee0c8
Savannah Bennett <savvy.bennett8@gmail.com>
2021-06-14 19:39:48 -0600

    user profile setup

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,31 +1,46 @@
 import React from 'react';
 //retrieves the username from the URL & passes to 'useQuery' Hook
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 
 import ThoughtList from "../components/ThoughtList";
 import FriendList from '../components/FriendList';
 
 import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from '../utils/auth';
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  //redirect to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Redirect to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>;
+  }
+
+  //if user tries to visit '/profile' but is not logged in:
+  if (!user.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to log in or sign up!
+      </h4>
+    );
   }
 
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {user.username}'s profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
       </div>
 
